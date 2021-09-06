93aea04798615059e63624ae296de3c0e36cf48f
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-06 11:34:54 -0500

    Creando clase electrodomesticos,nevera,telivisor y factura

---
+++ codigo.js
@@ -0,0 +1,12 @@
+//Inventario 
+
+    var generalConBNacio =  new Array (1,2,3,4);
+    var neveraConCNacio = new Array(1,2,3,4,5,6,7,8,9);
+    var TvsConCInter = new Array(1,2,3,4,5,6);
+    var TvsConBInter = new Array(1,2,3,4,5,6,7,8,9,10,11,12);
+    var TvsConANacio = new Array(1,2);
+    var generalAInter= new Array(1,2,3,4,5,6,7);
+    var neverasConCInter= new Array(1,2,3,4,5,6,7,8,9,10,11);
+
+    document.write(generalConBNacio.length + "Electrodomesticos generales de consumo B nacionales\n" + 
+    neveraConCNacio.length + "NEveras de consumos A nacionales")
